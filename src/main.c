7d53960d87e95f96d7eaabee1b8074de1dae672c
omeletteaufromage <76499744+omeletteaufromage@users.noreply.github.com>
2024-09-20 00:21:40 +0200

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,30 @@
+include Adafruit_NeoPixel.h
+
+// How many internal neopixels do we have? some boards have more than one!
+#define NUMPIXELS        1
+
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
+// the setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(115200);
+
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.setBrightness(20); // not so bright
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // say hi
+  Serial.println("Hello!");
+  
+  // set color to red
+  pixels.fill(0xFF0000);
+  pixels.show();
+  delay(500); // wait half a second
+
+  // turn off
+  pixels.fill(0x000000);
+  pixels.show();
+  delay(500); // wait half a second
+}
